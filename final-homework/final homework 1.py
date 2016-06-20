d2663538b9a41b1978a8159eb6280d95c337145d
NABLAfai <1321674548@qq.com>
2016-06-20 23:56:18 +0800

    Create final homework 1.py

---
+++ final-homework/final homework 1.py
@@ -0,0 +1,16 @@
+from __future__ import division
+import random
+import time
+
+for j in range(2, 8):
+    startT = time.clock()
+    counter = 0
+    for i in range(10 ** j):
+        x = random.uniform(-1, 1)
+        y = random.uniform(-1, 1)
+        if x**2 + y**2 < 1:
+            counter = counter + 1
+    endT = time.clock()
+    print (4 * (counter / 10 ** j))
+    print (endT - startT)
+    print "-"*15
